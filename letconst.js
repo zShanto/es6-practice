5765884848fab52db96fb3270721fb5e181b5bc7
Kz Shanto <kzshanto99@gmail.com>
2024-09-17 21:31:21 +0600

    knocking at ES6

---
+++ letconst.js
@@ -0,0 +1,11 @@
+const hubby = 'Elias Kanchon'
+console.log(hubby);
+
+let patientName = 'Rahim Chacha'
+patientName = 'Fatema Khala'
+console.log(patientName);
+let sum = 0; 
+for (let i = 0; i < 10; i++){
+    sum = sum + i;
+    console.log(i);
+}
---
+++ template.js
@@ -0,0 +1,11 @@
+const firstName = 'Justin'
+const lastName = 'TimberLake'
+const fullName = firstName +' ' + lastName + ' is a good boy';
+const fullName2 = `${firstName} ${20+60+20} is a good boy`;
+
+const multiLine = `I love you 
+I miss you
+no. I don't need you
+Baily road a dorkar nai`
+
+console.log(multiLine)
